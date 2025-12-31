f8580f630b2aed7eb41237d404f39a5546b6818e
Abdullahi Salaudeen <abdullahisalaudeen65@gmail.com>
2025-12-31 12:15:04 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,79 @@
 import Image from "next/image";
+import {
+  FaBarsStaggered,
+  FaRegCirclePlay,
+  FaRegStar,
+  FaStar,
+} from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div>
+      <div className="bg-linear-to-r/shorter from-emerald-100 to-white min-h-screen ">
+        <header className="px-8 py-6 flex item-center justify-between md:px-10 lg:px-25 lg:py-8 ">
+          <div className="text-emerald-400 font-semibold text-2xl">BICCAS</div>
+          <nav className="hidden md:block">
+            <ul className="flex gap-8 text-sm font-medium text-gray-400 lg:gap-14 lg:text-base">
+              <li className="text-black">Home</li>
+              <li>Product</li>
+              <li>FAQ</li>
+              <li>Blog</li>
+              <li>About Us</li>
+            </ul>
+          </nav>
+          <div className="md:hidden block text-2xl text-emerald-400">
+            <FaBarsStaggered />
+          </div>
+        </header>
+        <main className="px-8 md:flex w-full md:px-10 lg:px-25 ">
+          <section className="text-center w-full md:mt-6 md:w-6/12">
+            <h1 className=" font-bold text-gray-800 text-4xl px-3.5 lg:px-20 my-4 leading-12 md:leading-15 tracking-wide  lg:text-6xl">
+              We’re here to Increase your Productivity
+            </h1>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/line.svg"
+              alt="Line"
+              width={340}
+              height={34}
+              className="mx-auto"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <p className="my-8 text-md px-4 md:text-left lg:px-30 lg:text-sm leading-6">
+              Let`s make your work more organized and easier using the Taskio
+              Dashboard with many of the latest features in managing work every
+              day.
+            </p>
+            <div className="flex gap-6 justify-center mx-auto md:ml-30 md:justify-start my-8">
+              <button className="bg-emerald-400 text-white text-sm px-4 py-2 rounded-4xl">
+                Try free trial
+              </button>
+              <button className="flex items-center gap-2">
+                <span className="text-lg">
+                  <FaRegCirclePlay />
+                </span>
+                View demo
+              </button>
+            </div>
+          </section>
+          <section className="w-full md:w-6/12 my-16 md:my-0 md:mt-6 ">
+            <Image src="/test.svg" alt="Test Image" width={600} height={500} />
+          </section>
+        </main>
+
+        <div>
+          <div className="font-bold text-stone-900 text-2xl text-center my-12 lg:text-3xl">
+            <p>More than 25,000 teams use Collabs</p>
+          </div>
+          <div className="pb-12">
+            <Image
+              src="/brands.svg"
+              alt="Brands"
+              width={700}
+              height={100}
+              className="mx-auto text-4xl"
+            />
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
